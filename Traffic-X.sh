cd2f196b3bbd301cbedb7d04aebc5d32f91d0d42
Tyga-x <wishvajay02@gmail.com>
2025-09-30 18:30:15 +0530

    Traffic-X.sh

--- Traffic-X.sh
+++ Traffic-X.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-# @fileOverview Traffic-X Installer/Uninstaller
+# @fileOverview Check usage stats of X-SL
 # @author MasterHide
+# @Copyright © 2025 x404 MASTER™
 # @license MIT
+#
+# You may not reproduce or distribute this work, in whole or in part, 
+# without the express written consent of the copyright owner.
+#
+# For more information, visit: https://t.me/Dark_Evi
+
 
 set -euo pipefail
 
